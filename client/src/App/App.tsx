0e9be96d97c26df85376e3bf1923cc48dcc0396c
Robin Blanquart <rob.blanquart02100@gmail.com>
2023-09-17 17:08:19 +0200

    [ADD] setting up React Query + redux toolkit

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <div>
-            <p>App PPPP</p>
+            <p>App</p>
         </div>
     );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,16 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App/App.tsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+export const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+            <App />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </Provider>
+    </QueryClientProvider>
 );
