1a39920d0f82608b403f8ad4bfb4f063f21bb514
Robin Blanquart <rob.blanquart02100@gmail.com>
2023-09-17 20:00:01 +0200

    [ADD] setting up mongoDB server

---
+++ server/src/config/db.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectedDB = () => {
+  mongoose
+    .connect(process.env.LOCAL_DB_URL)
+    .then((con) =>
+      console.log(
+        `MongoDB connected with SUCCESS on host : ${con.connection.host}`
+      )
+    );
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
+import mongoose, { ConnectOptions } from "mongoose";
 const port = 8000;
 const app = express();
+import { config } from "dotenv";
+import { connectedDB } from "./config/db.js";
+
+config();
+connectedDB();
 
 app.listen(port, () => {
   console.log(`now listening on port ${port}`);
