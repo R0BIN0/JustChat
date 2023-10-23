a326487da097ea0efd4f86600792df7d12e100fc
Robin Blanquart <rob.blanquart02100@gmail.com>
2023-10-23 08:35:22 +0200

    [MODIFY] remove switch on catchError utils

--- client/src/utils/catchError.ts
+++ client/src/utils/catchError.ts
@@ -18,34 +18,21 @@ export const catchError = (err: any): IError => {
 };
 
 const getMessageError = (err: IErrorCode): string => {
-  switch (err) {
-    case IErrorCode.EMPTY_INPUT:
-      return "Le formulaire que vous avez envoyé est incorrect.";
-    case IErrorCode.USER_NOT_FOUND:
-      return "Adresse mail incorrect.";
-    case IErrorCode.INVALID_PASSWORD:
-      return "Mot de passe incorrect.";
-    case IErrorCode.CANNOT_GET_JWT_TOKEN:
-      return "Impossible de vous authentifier. Réessayer ultérieurement.";
-    case IErrorCode.CANNOT_CONFIRM_PASSWORD:
-      return "La confirmation de vos mots de passe est invalide.";
-    case IErrorCode.CANNOT_CREATE_USER:
-      return "Impossible de créer l'utilisateur. Veuillez réessayer plus tard.";
-    case IErrorCode.NAME_ALREADY_USED:
-      return "Ce nom d'utilisateur est déjà utilisé.";
-    case IErrorCode.SAME_EMAIL:
-      return "Cet email est déjà utilisé.";
-    case IErrorCode.WRONG_MAIL_FORMAT:
-      return "Votre adresse mail est invalide";
-    case IErrorCode.NO_USER:
-      return "Aucun utilisateur n'a été trouvé.";
-    case IErrorCode.CANNOT_CREATE_CHAT:
-      return "Impossible d'établir la communication. Veuillez recharger la page.";
-    case IErrorCode.NO_CHAT_FOUND:
-      return "Impossible d'établir la communication. Veuillez recharger la page.";
-    case IErrorCode.USERS_NOT_FOUND:
-      return "Impossible d'établir la communication. Veuillez recharger la page.";
-    default:
-      return "Une erreur est survenue. Veuillez réessayer ultérieurement.";
-  }
+  const map = {
+    [IErrorCode.EMPTY_INPUT]: "Le formulaire que vous avez envoyé est incorrect.",
+    [IErrorCode.USER_NOT_FOUND]: "Adresse mail incorrect.",
+    [IErrorCode.INVALID_PASSWORD]: "Mot de passe incorrect.",
+    [IErrorCode.CANNOT_GET_JWT_TOKEN]: "Impossible de vous authentifier. Réessayer ultérieurement.",
+    [IErrorCode.CANNOT_CONFIRM_PASSWORD]: "La confirmation de vos mots de passe est invalide.",
+    [IErrorCode.CANNOT_CREATE_USER]: "Impossible de créer l'utilisateur. Veuillez réessayer plus tard.",
+    [IErrorCode.NAME_ALREADY_USED]: "Ce nom d'utilisateur est déjà utilisé.",
+    [IErrorCode.SAME_EMAIL]: "Cet email est déjà utilisé.",
+    [IErrorCode.WRONG_MAIL_FORMAT]: "Votre adresse mail est invalide.",
+    [IErrorCode.NO_USER]: "Aucun utilisateur n'a été trouvé.",
+    [IErrorCode.CANNOT_CREATE_CHAT]: "Impossible d'établir la communication. Veuillez recharger la page.",
+    [IErrorCode.NO_CHAT_FOUND]: "Impossible d'établir la communication. Veuillez recharger la page.",
+    [IErrorCode.USERS_NOT_FOUND]: "Impossible d'établir la communication. Veuillez recharger la page.",
+    [IErrorCode.UNEXCPECTED_ERROR]: "Une erreur est survenue. Veuillez réessayer ultérieurement.",
+  };
+  return map[err] ?? "Une erreur est survenue. Veuillez réessayer ultérieurement.";
 };
